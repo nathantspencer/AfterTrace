559c6f069f2075ffacfed431c6ec6d4a6f3ca959
nathantspencer <nathantspencer@gmail.com>
2017-10-30 18:27:49 -0400

    comment headers for color groups in stylepainter

--- sources/GUI/StylePainter.cpp
+++ sources/GUI/StylePainter.cpp
@@ -3,13 +3,20 @@
 StylePainter::StylePainter() :
     m_colors(std::map<std::string, ImVec4>())
 {
+    //======| Grays |============================================
     m_colors["gray"]        = ImVec4(0.22f, 0.22f, 0.23f, 1.00f);
     m_colors["darkGray"]    = ImVec4(0.11f, 0.12f, 0.15f, 1.00f);
+    
+    //======| Purples |==========================================
     m_colors["purple"]      = ImVec4(0.25f, 0.25f, 0.40f, 1.00f);
     m_colors["lightPurple"] = ImVec4(0.28f, 0.28f, 0.43f, 1.00f);
+    
+    //======| Whites |===========================================
     m_colors["whiteShadow"] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
-    m_colors["blackShadow"] = ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
     m_colors["offWhite"]    = ImVec4(0.81f, 0.81f, 0.82f, 1.00f);
+    
+    //======| Blacks |===========================================
+    m_colors["blackShadow"] = ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
 }
 
 StylePainter::~StylePainter()
