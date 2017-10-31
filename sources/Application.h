cc0fa16af9aaff815a5be4e5539500782a3a6e55
nathantspencer <nathantspencer@gmail.com>
2017-10-31 15:06:27 -0400

    Added Doxygen comments for Application class + Doxygen comments for Application.h

--- sources/Application.h
+++ sources/Application.h
@@ -6,13 +6,31 @@
 #include "GUI/Window/AdjustmentWindow.h"
 #include "GUI/Window/DataWindow.h"
 
+/*! The main application class.
+ *
+ *  While not strictly enforced, this class is to be used as a singleton.
+ */
 class Application
 {
     public:
     
+        /*! The constructor for `Application`.
+         *
+         *  @param glfwWindow a pointer to the `GLFWwindow` in which the application runs
+         */
         Application(GLFWwindow* glfwWindow);
+    
+        /*! The destructor for `Application`.
+         */
         ~Application();
     
+        /*! Main application update call.
+         *
+         *  The update functions of other classes owned by `Application` are called within this function,
+         *  including GUI elements.
+         *
+         *  @param deltaSeconds the `float` number of seconds since the last update
+         */
         void    Update(float deltaSeconds);
     
         GLFWwindow* GetGlfwWindow();
